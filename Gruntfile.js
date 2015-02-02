c0302f534fa67d4b5dfcf227d194a06dba116c29
Janic Beauchemin <jbeauchemin@sidlee.com>
2015-02-02 15:23:23 -0500

    add imagemin grunt task add scss mixin for animation

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,4 +33,5 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build:css', ['sass_imports', 'sass', 'autoprefixer', 'csswring']);
     grunt.registerTask('build:js', ['jshint', 'browserify']);
+    grunt.registerTask('imageMin', ['imagemin']);
 };
