9201b141dbf483942bf309721d5e8eeeed6a55c5
Janic Beauchemin <jbeauchemin@u92.ca>
2015-06-11 16:49:33 -0400

    retina sprite

--- grunt/sprite.js
+++ grunt/sprite.js
@@ -1,9 +1,11 @@
 module.exports = {
     all: {
-        cssFormat: 'css',
+        cssFormat: 'css_retina',
         dest: '<%= imgPath %>/sprite.png',
         destCss: '<%= cssSrc %>/sprite.scss',
         imgPath: '../../img/sprite.png',
+        retinaDest: '<%= imgPath %>/sprite@2x.png',
+        retinaSrcFilter: ['<%= imgPath %>/sprite/*@2x.png'],
         src: '<%= imgPath %>/sprite/*.png'
     }
 };
